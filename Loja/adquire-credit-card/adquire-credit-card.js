b40eab205b09415f6e9db3d134abd599f3084304
Heberty <hebertyprado22005@gmail.com>
2022-03-27 13:50:51 -0300

    att

--- Loja/adquire-credit-card/adquire-credit-card.js
+++ Loja/adquire-credit-card/adquire-credit-card.js
@@ -9,3 +9,42 @@ if(logado){
 else{
     administrador.innerText = "Login";
 }
+
+
+/*  Make a Credit Card Payment Form Using HTML */
+
+document.querySelector("#nome").oninput = () => {
+    document.querySelector('.name-card-people').innerText = document.querySelector('#nome').value;
+}
+
+document.querySelector("#number").oninput = () => {
+    document.querySelector('.number-card').innerText = document.querySelector('#number').value;
+}
+
+document.querySelector("#day").oninput = () => {
+    document.querySelector('.campo-data-day').innerText = document.querySelector('#day').value;
+}
+
+document.querySelector("#month").oninput = () => {
+    document.querySelector('.campo-data-month').innerText = document.querySelector('#month').value;
+}
+
+document.querySelector("#month").oninput = () => {
+    document.querySelector('.campo-data-month').innerText = document.querySelector('#month').value;
+}
+
+document.querySelector("#csv").onmouseenter = () => {
+    document.querySelector('.adquire-credit-card').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.credit-card-back').style.transform = 'perspective(1000px) rotateY(0deg)'; 
+    document.querySelector('.input-card-back-cvv').innerText = document.querySelector('#csv').value;
+}
+
+document.querySelector("#csv").oninput = () => {
+    document.querySelector('.input-card-back-cvv').innerText = document.querySelector('#csv').value;
+}
+
+document.querySelector("#csv").onmouseleave = () => {
+    document.querySelector('.adquire-credit-card').style.transform = 'perspective(1000px) rotateY(0deg)';
+    document.querySelector('.credit-card-back').style.transform = 'perspective(1000px) rotateY(180deg)'; 
+    document.querySelector('.input-card-back-cvv').innerText = document.querySelector('#csv').value;
+}
--- Loja/principal/principal.js
+++ Loja/principal/principal.js
@@ -11,3 +11,79 @@ else{
 }
 
 
+
+/*  textos automaticos  */
+
+var p = document.querySelectorAll(".pre-box-info");
+
+var array = new Map();
+var contador = 1; 
+var i = 0;
+
+array.set(1, "adquira cartao já");
+array.set(2, "cartão gratis");
+array.set(3, "Nada de dificil");
+array.set(4, "É muito facil");
+array.set(5, "obtenha descontos");
+array.set(6, "Venha sempre");
+
+
+
+function retorno(texto){
+    for(i = 0; i < texto.length; i++){
+        p[i].innerText = texto[i];
+    }
+}
+
+function eraser(){
+    for(i = 0; i < p.length; i++){
+        p[i].innerText = "";
+    }
+}
+
+
+setInterval(tempoAntigido, 1000*2);
+
+
+
+
+    function tempoAntigido(){
+        
+        if(contador == 1){
+            eraser();
+            retorno(array.get(1));
+            
+        }
+        else if(contador == 2){
+            eraser();
+            retorno(array.get(2));
+
+        }
+
+        else if(contador == 3){
+            eraser();
+            retorno(array.get(3));
+
+        }
+
+        else if(contador == 4){
+            eraser();
+            retorno(array.get(4));
+
+        }
+
+        else if(contador == 5){
+            eraser();
+            retorno(array.get(5));
+
+        }
+
+        else{
+            eraser();
+            retorno(array.get(6));
+            contador = 0;
+        }
+        contador = contador + 1;
+    }
+
+
